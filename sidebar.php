fbe36f2e57de3c1c8a7bce87752a29cdc11e505f
joen <joen@d957f892-c61d-0410-b221-f235e6eecf30>
2016-12-16 09:18:23 +0000

    P2: Improve responsiveness, some code cleanup
    Summary:
    Edit the CSS of O2 to improve the responsiveness.
    
    - Instead of moving widgets in and out of the sidebar, just move the navigation. This can be improved even further.
    - Use a traditional, bigger, hamburger on the left, and animate with CSS for performance
    - Teensy tweaks to the O2 visuals plus code cleanups. Round avatars, and more consistent margins
    - Move to `box-sizing: border-box;`, as this allows us to kill off a bunch of `calc`'s
    
    Test Plan:
    - Test the vanilla O2 and see it works correctly on desktops and mobile phones
    - Sandbox various internal P2s and see that any design customizations don't break, or break too badly
    
    Reviewers: pento
    
    Subscribers: obenland
    
    Differential Revision: D3673

--- sidebar.php
+++ sidebar.php
@@ -9,7 +9,6 @@ if( ! is_active_sidebar( 'sidebar-1' ) )
 	return;
 ?>
 	<div id="secondary" class="widget-area" role="complementary">
-		<a href="#" id="secondary-toggle"></a>
 		<div id="secondary-content">
 			<?php do_action( 'before_sidebar' ); ?>
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
